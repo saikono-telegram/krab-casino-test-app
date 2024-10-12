d81cba7f21edab3c557429f72c31129422541007
Saikono <111527317+saikono-telegram@users.noreply.github.com>
2024-10-13 02:19:25 +0600

    Add files via upload

---
+++ js/telegram.js
@@ -0,0 +1,17 @@
+let tg = window.Telegram.WebApp; 
+
+tg.expand(); 
+
+const card = document.getElementById('user');
+
+const newCard = document.createElement('div');
+
+newCard.innerHTML =  
+`
+<div class="wrap-menu">
+    <span class="label-casino">Ваш Telegram ID: #${tg.initDataUnsafe.user.id}</span>
+    <span class="focus-icon" data-symbol="&#xf207;"></span>     
+</div>
+`;
+
+card.appendChild(newCard);
